999d0270fb4b059d12f8bcfd85ccad0d6fe97f59
Stephanie Pena <stephanie.pena@stephanies-mbp.myfiosgateway.com>
2020-10-12 23:57:43 -0400

    phone number encryption function

--- encryptPhone.js
+++ encryptPhone.js
@@ -5,4 +5,40 @@
  */
 
 
-module.exports = encryptPhoneNumber;
+function encryptPhoneNumber(phoneNumInput) {
+  let phoneNumString = phoneNumInput.toString();
+  let phoneNumSections = [];
+  let phoneNumSectAsterisk = [];
+  let phoneNumSectDash = [];
+  
+  
+  for (let i = 0, sectionLength = phoneNumString.length; i < sectionLength - 4; i += 3) {
+    phoneNumSections.push(phoneNumString.substring(i, i + 3));
+}
+  for (let j = 6, sectionLength = phoneNumString.length; j < sectionLength; j += 4) {
+   phoneNumSections.push(phoneNumString.substring(j, j + 4));
+}     
+  phoneNumSectAsterisk = phoneNumSections;
+  phoneNumSectAsterisk[0] = '***';
+//  phoneNumSectAsterisk[1] = '***';
+  
+ // for (let k = 0; k < phoneNumSections.length; k+= 2) {
+ //   phoneNumSectDash.push(phoneNumSections[1], '-');
+//  }
+  
+  //for (let l = 2; l < phoneNumSections.length; l+= 2) {
+   //phoneNumSectDash.push(phoneNumSections[4], '-');
+ // }
+  
+  //param = phoneNumSections.join(" ");
+
+  return console.log(phoneNumSections);
+  
+}
+
+encryptPhoneNumber(2223334444)
+  //let phoneNumSections = phoneNumString.match(/.{1,3}/g);
+  //let param = phoneNumSections.join("-");
+
+  //return console.log(phoneNumSections)
+ 
